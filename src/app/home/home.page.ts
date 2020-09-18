7e804763c04ff0212463e7e5480313934d3439be
swapnil shukla <swapnil.shukla1508@gmail.com>
2020-09-18 18:57:12 +0530

    Initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { isNumber } from 'util';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,55 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+  value ='0';
+  readyFornewinput = true;
+  oldValue ='0';
+  lastOperator ='';
+  numberGroups =[
+   [7,8,9,'x']
+  ,[4,5,6,'-']
+  ,[1,2,3,'+']
+  ,[0,'c','/','=']];
+  OnButtonPress(symbol)
 
+  {
+    console.log(symbol);
+    if(isNumber(symbol))
+    {
+      console.log("is a number");
+      if(this.readyFornewinput)
+      {
+        this.value = ''+symbol;
+      }else
+      {
+        this.value += '' + symbol;
+      }
+      this.readyFornewinput =false;
+    }
+    else if(symbol === 'c')
+    {
+      this.value ='0';
+      this.readyFornewinput = true;
+    }else if(symbol ==='=')
+    {
+      if(this.lastOperator === 'x')
+      this.value ='' +(parseInt(this.oldValue )* parseInt(this.value));
+      else if(this.lastOperator === '+')
+      this.value = ''+ (parseInt(this.oldValue) +parseInt(this.value));
+      else if(this.lastOperator === '/')
+      this.value =''+(parseInt(this.oldValue)/parseInt(this.value));
+      else if(this.lastOperator === '-') 
+      this.value = ''+(parseInt(this.oldValue) - parseInt(this.value));
+      this.readyFornewinput = true;
+
+
+    }
+    else{
+      this.readyFornewinput = true;
+      this.oldValue =this.value;
+      this.lastOperator = symbol;
+    }
+      
+    }
 }
+
